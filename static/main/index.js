b399476cdc55c341c16e472d5553714b667013ac
Benqqa <53792741+Benqqa@users.noreply.github.com>
2021-04-12 09:55:19 +0300

    header

---
+++ static/main/index.js
@@ -0,0 +1,16 @@
+new Vue({
+    el: '#app',
+    vuetify: new Vuetify(),
+     data: {
+      checkbox1: true,
+      min: -50,
+      max: 90,
+      range: [-20, 70],
+     },
+     mounted() {
+
+     },
+     methods: {
+
+     },
+});
