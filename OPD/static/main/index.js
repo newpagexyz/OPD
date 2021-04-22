e3214517d27501ec909992e00fe79e72638e4354
Benqqa <53792741+Benqqa@users.noreply.github.com>
2021-04-22 14:06:17 +0300

    ended html + css to auth/info/main

--- OPD/static/main/index.js
+++ OPD/static/main/index.js
@@ -7,11 +7,21 @@ new Vue({
       min: -50,
       max: 90,
       range: [-20, 70],
+      hides: [1,1,0,0,0],
+      selected: ['John'],
+
      },
      mounted() {
 
      },
      methods: {
-
+      Hiden: function(Num){
+        var Arr= this.hides
+        Arr[Num]= this.hides[Num] == 1? 0: 1;
+        this.hides=[]
+        Arr.forEach(el =>{
+          this.hides.push(el)
+        })
+      }
      },
 });
