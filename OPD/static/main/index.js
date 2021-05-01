ed632c2a7ab3f95d7f4ed778abb3c628fea02d58
Benqqa <53792741+Benqqa@users.noreply.github.com>
2021-05-01 20:20:21 +0300

    view list ADC

--- OPD/static/main/index.js
+++ OPD/static/main/index.js
@@ -2,19 +2,39 @@ new Vue({
     el: '#app',
     vuetify: new Vuetify(),
      data: {
-      src: '/OPD/static',
+      token: '',
+      session: '',
+      src: '/static',
       checkbox1: true,
       min: -50,
       max: 90,
       range: [-20, 70],
-      hides: [1,1,0,0,0],
+      hides: [
+      1,1,0,0,0
+      ],
       selected: ['John'],
+      ARCs: [],
 
      },
      mounted() {
-
+        this.token=this.getCookie('token')
+        this.session=this.getCookie('session')
+        // проверка на наличие куков
+        if (this.token == undefined || this.token == "empty") {
+          window.location.href = `/static/auth/`
+        }
+        this.GetADC()
      },
      methods: {
+      GoToInfo: function(id__name){
+        localStorage.id = id__name;
+        window.location.href = '/static/info/'
+      },
+      OpenTech: function(id_name){
+        if(this.ARCs[id_name].tech != null){
+         window.location.href = `https://adc.newpage.xyz/file/tech/`+this.ARCs[id_name].tech
+        }
+      },
       Hiden: function(Num){
         var Arr= this.hides
         Arr[Num]= this.hides[Num] == 1? 0: 1;
@@ -22,6 +42,37 @@ new Vue({
         Arr.forEach(el =>{
           this.hides.push(el)
         })
-      }
+      },
+      GetADC: function(c_mane){
+        fetch('https://adc.newpage.xyz/api/search_adc/?max_INL_max=10000000000000/').then(res => res.json())
+                .then(resJson => {
+                  this.ARCs =  resJson;
+                  console.log(resJson);
+
+                })
+      },
+      Exit:function(c_name){
+        this.deleteCookie(this.session);
+        this.deleteCookie(this.token);
+        document.cookie = "token="+'empty'+"; path=/; ";
+        document.cookie = "session="+'empty'+"; path=/; ";
+        window.location.href = `/static/auth/`
+      },
+      ///
+      //работа с куки
+      getCookie: function(c_name){
+        var i,x,y,ARRcookies=document.cookie.split(";");
+        for (i=0;i<ARRcookies.length;i++)
+        {
+           x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+           y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+           x=x.replace(/^\s+|\s+$/g,"");
+           if (x==c_name)
+           {
+               return unescape(y);
+           }
+        }
+      },
      },
+      
 });
